8c2338692e4a66d78c19cc055760d08d6a34e9ab
Connor J Hainley <ch983@pvhs-112-d20.pvsd.org>
2017-11-03 08:50:16 -0400

    Initial commit

---
+++ src/com/company/HighSchoolClass.java
@@ -0,0 +1,26 @@
+package com.company;
+
+/**
+ * Created by ch983 on 11/3/17.
+ */
+public class HighSchoolClass {
+    private Student [] students;
+    //constructors and other data fields not shown
+//returns the valedictorian of the class
+    public Student getValedictorian()
+    {
+// to be implemented
+    }
+    // returns the percentage of students in the
+// honors program
+    public double getHonorsPercentage()
+    {
+// to be implemented
+    }
+}
+    //The following are some of the public methods of the Student class:
+// returns the student's grade point average
+public double getGPA()
+// returns true if the student is in the honors program
+public boolean isHonors()
+}
