57e467573d3ba945b623006fa434576aa21c7265
chainley56 <cjhainley@gmail.com>
2017-11-05 23:46:55 -0500

    update

--- src/com/company/HighSchoolClass.java
+++ src/com/company/HighSchoolClass.java
@@ -3,6 +3,11 @@ package com.company;
 /**
  * Created by ch983 on 11/3/17.
  */
+/* To be honest I'm not sure where to start I know you have to find the highest GPA then return that student with the
+highest GPA as the valedictorian. Then for the other one you find the amount of trues that come from the boolean
+and then put that over the total amount of students and then multiply by 100 and that is your percentage. I would do
+this if I knew how but I don't.*/
+ */
 public class HighSchoolClass {
     private Student [] students;
     //constructors and other data fields not shown
